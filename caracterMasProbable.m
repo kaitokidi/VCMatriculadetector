b42dd584f8934c94abe56eeddc2866ee7cb105df
kaitokidig <genis.bayona@est.fib.upc.edu>
2015-11-25 18:55:04 +0100

    tengo el comit pequenio puta bida m8

--- caracterMasProbable.m
+++ caracterMasProbable.m
@@ -1,4 +1,10 @@
 function caracter = caracterMasProbable(clasificador,  features)
-caracter='a';
+ [characters,scores] = predict (clasificador, features);
+ max(scores(1:end))
+ if max(scores(1:end)) >= 0.25
+    caracter = characters(1,1);
+ else
+    caracter = 'Ñ';
+ end
 end
 
